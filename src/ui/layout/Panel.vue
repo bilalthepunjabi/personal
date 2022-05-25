8c196514f72732b47ab0a82671153011b8453f13
Bilal Punjabi <bilalthepunjabi@gmail.com>
2022-05-25 17:42:46 +0000

    --ui --panel

---
+++ src/ui/layout/Panel.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+
+export default defineComponent({
+  name: "Panel",
+  props: {
+    
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>
+
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+    <div class="px-4 py-5 sm:px-6">
+      <!-- Content goes here -->
+      <!-- We use less vertical padding on card headers on desktop than on body sections -->
+      <slot name="header"></slot>
+    </div>
+    <div class="px-4 py-5 sm:p-6">
+      <!-- Content goes here -->
+      <slot></slot>
+    </div>
+    <div class="px-4 py-4 sm:px-6">
+      <!-- Content goes here -->
+      <!-- We use less vertical padding on card footers at all sizes than on headers or body sections -->
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Panel from "../ui/layout/Panel.vue"
+</script>
 
 <template>
-  <main>Hello World</main>
+  <main>
+    <Panel>
+      <template #header>
+        <h1>Here might be a page title</h1>
+      </template>
+      
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+
+      <template #footer>
+        <p>Here's some contact info</p>
+      </template>
+    </Panel>
+  </main>
 </template>
